302b1d41a4e048cb9be59f77a8ed7f8100fcc373
Wilhelm Wanecek <wilhelm.wanecek@gmail.com>
2018-03-31 21:09:40 +0200

    Refactor to use webpack in preparation for ThreeJS stuff

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const Autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [Autoprefixer(), cssnano()],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
